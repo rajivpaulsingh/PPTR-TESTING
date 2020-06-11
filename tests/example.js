de1c1dea83858d63dbceaac05625ef84b58f2342
Singh <singh2rajiv@dgb-c02vh08bh.lan>
2020-06-11 10:09:19 -0700

    Implemented the type method

--- tests/example.js
+++ tests/example.js
@@ -3,6 +3,7 @@ const expect = require('chai').expect
 
 const config = require('../lib/config')
 const click = require('../lib/helpers').click
+const typeText = require('../lib/helpers').typeText
 
 describe('Example Test', () => {
 
@@ -42,5 +43,14 @@ describe('Example Test', () => {
         await page.goto(config.baseUrl)
         await click(page, '#signin_button')
         await page.waitForSelector('#user_login')
+    })
+
+    it('Type method', async () => {
+        await page.goto(config.baseUrl)
+        await click(page, '#signin_button')
+        await typeText(page, 'username', '#user_login')
+        await typeText(page, 'password', '#user_password')
+        await click(page, '.btn-primary')
+        await page.waitForSelector('.nav-tabs')
     })
 })
