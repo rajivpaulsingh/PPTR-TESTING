bc29f93b99cffe6b757582a6b67fa8cc3f603114
Singh <singh2rajiv@dgb-c02vh08bh.bcg.com>
2020-06-11 10:59:09 -0700

    Utility to generate random email

--- tests/example.js
+++ tests/example.js
@@ -9,6 +9,7 @@ const waitForText = require('../lib/helpers').waitForText
 const pressKey = require('../lib/helpers').pressKey
 const shouldExist = require('../lib/helpers').shouldExist
 const generateID = require('../lib/utils').generateID
+const generateEmail = require('../lib/utils').generateEmail
 
 describe('Example Test', () => {
 
@@ -68,6 +69,9 @@ describe('Example Test', () => {
         await pressKey(page, 'Enter')
         await waitForText(page, 'body', 'The following pages were found for the query')
         await typeText(page, generateID(15), '#searchTerm')
+        await pressKey(page, 'Enter')
+        await waitForText(page, 'body', 'No results were found for the query')
+        await typeText(page, generateEmail(), '#searchTerm')
         await pressKey(page, 'Enter')
         await waitForText(page, 'body', 'No results were found for the query')
     })
